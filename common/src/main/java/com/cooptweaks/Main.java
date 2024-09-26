88f494a2f1110c25bb618fcaebd6d91c93cf33af
Kyagara <kyagara@proton.me>
2024-09-26 10:26:52 -0300

    shadowing discord4j in both loaders, reimpl of all events

--- common/src/main/java/com/cooptweaks/Main.java
+++ common/src/main/java/com/cooptweaks/Main.java
@@ -3,10 +3,9 @@ package com.cooptweaks;
 import com.cooptweaks.advancements.Advancements;
 import com.cooptweaks.discord.Discord;
 import com.cooptweaks.events.GrantCriterionCallback;
-import com.cooptweaks.events.PlayerDeathCallback;
 import dev.architectury.event.EventResult;
 import dev.architectury.event.events.common.*;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,7 +24,7 @@ public final class Main {
 	private static final ConcurrentHashMap<String, String> PLAYER_CURRENT_DIMENSION_ID = new ConcurrentHashMap<>();
 
 	public static void init() {
-		LifecycleEvent.SERVER_STARTING.register(server -> {
+		LifecycleEvent.SERVER_BEFORE_START.register(server -> {
 			DISCORD.Start(CONFIG.getDiscordConfig());
 		});
 
@@ -52,13 +51,6 @@ public final class Main {
 
 		PlayerEvent.PLAYER_QUIT.register(DISCORD::PlayerLeft);
 
-		/*PlayerEvent.PLAYER_RESPAWN.register((newPlayer, conqueredEnd, removalReason) -> {
-			String name = newPlayer.getName().getString();
-			String dimensionId = newPlayer.getWorld().getRegistryKey().getValue().toString();
-
-			updatePlayerCurrentDimension(name, dimensionId);
-		});*/
-
 		PlayerEvent.CHANGE_DIMENSION.register((player, oldWorld, newWorld) -> {
 			String name = player.getName().getString();
 			String dimensionId = newWorld.getValue().toString();
@@ -67,21 +59,24 @@ public final class Main {
 			DISCORD.PlayerChangedDimension(name, Utils.getPlayerDimension(name));
 		});
 
+		// Maybe use DECORATE event instead?
 		ChatEvent.RECEIVED.register((player, message) -> {
 			DISCORD.PlayerSentChatMessage(player, message);
 			return EventResult.pass();
 		});
 
-        EntityEvent.LIVING_DEATH.register((entity, source) -> {
-            if (entity.isPlayer()) {
-                String name = entity.getName().getString();
-                String dimensionId = entity.getWorld().getRegistryKey().getValue().toString();
+		EntityEvent.LIVING_DEATH.register((entity, source) -> {
+			if (entity.isPlayer()) {
+				String name = entity.getName().getString();
+				String dimensionId = entity.getWorld().getRegistryKey().getValue().toString();
+				BlockPos pos = entity.getBlockPos();
 
-                updatePlayerCurrentDimension(name, dimensionId);
-            }
+				updatePlayerCurrentDimension(name, dimensionId);
+				DISCORD.PlayerDied(name, pos, source.getDeathMessage(entity));
+			}
 
-            return EventResult.pass();
-        });
+			return EventResult.pass();
+		});
 
 		GrantCriterionCallback.EVENT.register(ADVANCEMENTS::OnCriterion);
 		CommandRegistrationEvent.EVENT.register(ADVANCEMENTS::RegisterCommands);
